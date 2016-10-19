7ca535ca9c6dc0fb2f201a154954882a71ec3a56
Charlie Hsu <hsu21609@gmail.com>
2016-10-19 23:25:19 +0800

    Abandon toLocalString

--- app/logger/index.js
+++ app/logger/index.js
@@ -7,7 +7,7 @@ var logger = new(winston.Logger)({
     new(winston.transports.File)({
       filename: path.join(process.env.LOG_ROOT || '', 'chat.log'),
       timestamp: function() {
-        return new Date().toLocaleString('zh-TW')
+        return (new Date() + 3600000 * 8).toString().substring(4, 24) //Use momentjs later
       }
     })
   ]
