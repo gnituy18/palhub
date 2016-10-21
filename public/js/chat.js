790714a4742c045aec6f0a5f79e464f80bb03139
Charlie Hsu <hsu21609@gmail.com>
2016-10-21 16:17:42 +0800

    Add stun servers

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,15 +14,22 @@
   var pc
   var peerConfig = {
     iceServers: [{
-      url: 'stun:stun.l.google.com:19302'
-    }, {
-      url: 'stun:stun1.l.google.com:19302'
-    }, {
-      url: 'stun:stun2.l.google.com:19302'
-    }, {
-      url: 'stun:stun3.l.google.com:19302'
-    }, {
-      url: 'stun:stun4.l.google.com:19302'
+      urls: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun3.l.google.com:19302',
+        'stun:stun4.l.google.com:19302',
+        'stun:stun.ekiga.net',
+        'stun:stun.ideasip.com',
+        'stun:stun.rixtelecom.se',
+        'stun:stun.schlund.de',
+        'stun:stun.stunprotocol.org:3478',
+        'stun:stun.voiparound.com',
+        'stun:stun.voipbuster.com',
+        'stun:stun.voipstunt.com',
+        'stun:stun.voxgratia.org'
+      ]
     }]
   }
   var constraints = {
