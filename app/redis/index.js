556fb430d63d7488e08650503cc2f1d83ff94475
Charlie Hsu <hsu21609@gmail.com>
2016-11-01 22:43:50 +0800

    Fix guard users bug

--- app/redis/index.js
+++ app/redis/index.js
@@ -2,6 +2,6 @@ var redis = require('redis')
 var promise = require('bluebird')
 promise.promisifyAll(redis)
 var client = redis.createClient()
-client.set('counter', '0')
+client.del('users')
 
 module.exports = client
