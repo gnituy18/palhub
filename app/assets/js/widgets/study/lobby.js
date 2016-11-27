7cfbe3278122d4e9bf9cd08005bc0f4a668f8d12
Charlie Hsu <hsu21609@gmail.com>
2016-11-27 15:38:37 +0800

    Rewrite sentences for study

--- app/assets/js/widgets/study/lobby.js
+++ app/assets/js/widgets/study/lobby.js
@@ -8,8 +8,10 @@
       console.log(tables)
       var str = ''
       for (var x in tables) {
-        str += '<a href="/study/' + tables[x].id + '">' + tables[x].subject + '</a><br>'
+        str += '<a class="study-table shadow-gray" href="/study/' + tables[x].id + '"><div >' + tables[x].subject + '</div></a><br>'
       }
+      if (str == '')
+        str = '<h1 class="font-gray">目前沒有人開設書桌</h1>'
       $('#tables').html(str)
     })
 
