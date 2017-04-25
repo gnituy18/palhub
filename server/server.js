bcbf7860bd2392d1846388a7920699677ddc4bdb
sourfeethsu <sourfeethsu@gmail.com>
2017-04-25 10:32:24 +0800

    Add koa-static

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const Koa = require('koa')
 const views = require('koa-views')
+const serve = require('koa-static')
 const path = require('path')
 const app = require('../app')
 const tour = require('../tour')
 const koa = new Koa()
 
 koa.use(views(path.join(__dirname, '..'), {'extension': 'pug'}))
+
+koa.use(serve(path.join(__dirname, '../public')))
 
 koa.use(app.routes())
 koa.use(tour.routes())
