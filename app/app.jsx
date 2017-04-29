ee2b40cfc0dd769f7588afc8f8d9d3ac4dbc66a1
sourfeethsu <sourfeethsu@gmail.com>
2017-04-29 16:12:59 +0800

    Add chat

--- app/app.jsx
+++ app/app.jsx
@@ -1,4 +1,91 @@
+const socket = io()
+
+class Room extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      msg: ['hi']
+    }
+    this.appendMsg = this.appendMsg.bind(this)
+  }
+
+  componentDidMount () {
+    socket.on('msg', this.appendMsg)
+  }
+
+  render () {
+    return (
+      <div>
+        <MessageBox msg={this.state.msg}/>
+        <InputBox/>
+      </div>
+    )
+  }
+
+  appendMsg(data){
+    this.setState((prevState, props) => {
+      return {
+        msg: prevState.msg.concat(data.value)
+      }
+    })
+  }
+}
+
+class MessageBox extends React.Component {
+  constructor (props) {
+    super(props)
+    console.log(this.props.msg)
+  }
+  
+  render () {
+    return (
+      <div>
+        {this.props.msg.map((m, index) => {
+          return (
+            <p key={index}>{m}</p>
+          )
+        })}     
+      </div>
+    ) 
+  }
+}
+
+class InputBox extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.sendMsg = this.sendMsg.bind(this)
+  }
+
+  render () {
+    return (
+      <div>
+        <input value={this.state.inputValue} onChange={this.handleChange} type='text'></input>
+        <button onClick={this.sendMsg}>submit</button>
+      </div>
+    )
+  }
+
+  handleChange (evt) {
+    this.setState({
+      inputValue: evt.target.value
+    })
+  }
+  
+  sendMsg () {
+    socket.emit('send msg', {
+      value: this.state.inputValue
+    })
+    this.setState({
+      inputValue: ''
+    })
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Room/>,
   document.getElementById('root')
 )
--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const http = require('http')
 const config = require('../config')
 const server = require('./server')
+const socketio = require('socket.io')
 
-http.createServer(server.callback()).listen(config.port)
+
+const serverHttp = http.createServer(server.callback())
+
+const io = socketio.listen(serverHttp)
+
+serverHttp.listen(config.port)
+
+io.on('connection', function(socket){
+  socket.on('send msg', function(data){
+    io.emit('msg', data)
+  })
+})
