75dc8c6ce1d8d326b39680e64afa3587cf13a06d
sourfeethsu <sourfeethsu@gmail.com>
2017-06-19 22:47:07 +0800

    Refactor web/lib/ajax

--- web/lib/ajax/index.js
+++ web/lib/ajax/index.js
@@ -1,18 +1,3 @@
-function post (url, data) {
-  return new Promise(resolve => {
-    const xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function () {
-      if (this.readyState === XMLHttpRequest.DONE) {
-        resolve({
-          'status': this.status,
-          'data': xhttp.response
-        })
-      }
-    }
-    xhttp.open('POST', url, true)
-    xhttp.setRequestHeader('Content-type', 'application/json')
-    xhttp.send(JSON.stringify(data))
-  })
-}
+import post from './post'
 
 export {post}
---
+++ web/lib/ajax/post.js
@@ -0,0 +1,16 @@
+export default function post (url, data) {
+  return new Promise(resolve => {
+    const xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === XMLHttpRequest.DONE) {
+        resolve({
+          'status': this.status,
+          'data': xhttp.response
+        })
+      }
+    }
+    xhttp.open('POST', url, true)
+    xhttp.setRequestHeader('Content-type', 'application/json')
+    xhttp.send(JSON.stringify(data))
+  })
+}
