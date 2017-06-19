bb995d0f6e48781f799ac086e21d336886a14940
sourfeethsu <sourfeethsu@gmail.com>
2017-06-19 20:52:03 +0800

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,43 @@
-var gulp = require('gulp')
-var browserify = require('browserify')
-var babelify = require('babelify')
-var source = require('vinyl-source-stream')
-var nodemon = require('nodemon')
-var sass = require('gulp-sass')
+const gulp = require('gulp')
+const browserify = require('browserify')
+const babelify = require('babelify')
+const source = require('vinyl-source-stream')
+const tap = require('gulp-tap')
+const path = require('path')
+const nodemon = require('nodemon')
+const sass = require('gulp-sass')
 
-const features = [ 'room', 'lobby', 'create' ]
+const features = require('./config').features
+const featureGlobStr = '(' + features.join('|') + ')'
 
-for (let x = 0;x < features.length;x++) {
-  const feature = features[x]
-  gulp.task(feature, function () {
-    return browserify('./web/' + feature + '/' + feature + '.jsx')
-    .transform(babelify, {'presets': [ 'es2015', 'react' ]})
-    .bundle()
-    .pipe(source(feature + '.js'))
-    .pipe(gulp.dest('./public/js'))
-  })
-}
-
-gulp.task('login', function () {
-  return browserify('./web/login/login.js')
-  .transform(babelify, {'presets': ['es2015']})
-  .bundle()
-  .pipe(source('login.js'))
-  .pipe(gulp.dest('./public/js'))
+gulp.task('javascript', function () {
+  return gulp.src(['./web/**/*' + featureGlobStr + '.js'], {'read': false})
+  .pipe(tap(function (file) {
+    file.contents = browserify(file.path, {'debug': true})
+      .transform(babelify, {'presets': [ 'es2015', 'react' ]})
+      .bundle()
+      .pipe(source(path.basename(file.path)))
+      .pipe(gulp.dest('./public/js'))
+  }))
 })
 
-gulp.task('build', [ 'login', 'room', 'lobby', 'create' ])
-
 gulp.task('sass', function () {
-  return gulp.src([ './web/lobby/lobby.scss', './web/room/room.scss', './web/scss/main.scss' ])
+  return gulp.src([ '.web/**/*' + featureGlobStr + '.scss', './web/scss/main.scss' ])
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./public/css'))
 })
 
-gulp.task('watch', [ 'sass', 'build' ], function () {
-  for (let x = 0;x < features.length; x++) {
-    console.log(x)
-    gulp.watch([ './web/' + features[x] + '/' + features[x] + '.jsx', './web/components/*.jsx', './web/lib/**/*.js' ], ['build'])
-  }
-
-  gulp.watch('./web/login/login.js', ['build'])
-  gulp.watch([ './web/room/room.scss', './web/lobby/lobby.scss', './web/scss/**/*.scss' ], ['sass'])
+gulp.task('watch', ['build'], function () {
+  gulp.watch([ './web/*' + featureGlobStr + '/!(index).js*(x)', './web/!' + featureGlobStr + '**/*.js' ], ['javascript'])
+  gulp.watch([ '.web/**/*' + featureGlobStr + '.scss', './web/scss/*.scss' ], ['sass'])
   nodemon({
     'script': 'server/index.js',
-    'ignore': [ 'gulpfile.js', 'room/!(index.js)' ],
+    'ignore': [ 'public/**/*', 'gulpfile.js', './web/*' + featureGlobStr + '/!(index).js*(x)', './web/!' + featureGlobStr + '**/*.js' ],
     'env': {'PORT': '3000'}
   })
 })
 
-gulp.task('default', [ 'sass', 'build', 'watch' ])
+gulp.task('build', [ 'javascript', 'sass' ])
+
+gulp.task('default', [ 'build', 'watch' ])
 
